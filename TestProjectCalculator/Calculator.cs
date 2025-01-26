5a6634d4ca4933a3cc2cb724f196e2521cbe1c07
Khilchenko Yurii <yu.kh414@gmail.com>
2025-01-26 23:30:36 +0200

    add implementation for Calculator class

--- TestProjectCalculator/Calculator.cs
+++ TestProjectCalculator/Calculator.cs
@@ -2,5 +2,23 @@
 
 public class Calculator
 {
+    public double Add(double firstNumber, double secondNumber)
+    {
+        return firstNumber + secondNumber;
+    }
 
+    public double Substract(double firstNumber, double secondNumber)
+    {
+        return firstNumber - secondNumber;
+    }
+
+    public double Multiply(double firstNumber, double secondNumber)
+    {
+        return firstNumber * secondNumber;
+    }
+
+    public double Divide(double firstNumber, double secondNumber)
+    {
+        return firstNumber / secondNumber;
+    }
 }
